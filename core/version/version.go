1e80a4d94c0545ceeaca0ab5653279c1f5e6297a
nnlgsakib <nnlgsakib@users.noreply.github.com>
2026-06-21 13:39:05 +0600

    bump: version to v0.4.0

--- core/version/version.go
+++ core/version/version.go
@@ -10,7 +10,7 @@ var (
 	// Version is the semantic version of the application.
 	// Can be overridden at build time via:
 	// -ldflags "-X github.com/nnlgsakib/membuss/core/version.Version=1.0.0"
-	Version = "0.3.0"
+	Version = "0.4.0"
 
 	// GitCommit is the git commit SHA.
 	// Can be overridden at build time via:
